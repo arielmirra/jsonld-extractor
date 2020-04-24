5fad0885fcf0f6fc71078f46729d8b12c45176e7
Ariel Mirra <ariel.mirra@ing.austral.edu.ar>
2020-04-24 18:26:26 -0300

    add movies db

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -1,5 +1,6 @@
 import IMDBRunner.{extractor, links}
 import models.extractors.{Extractor, ImdbExtractor, LaNacionExtractor}
+import utils.Database
 
 import scala.io.Source
 
@@ -7,16 +8,14 @@ import scala.io.Source
 object IMDBRunner extends App {
     val extractor = ImdbExtractor()
     val links = Source.fromFile("src/main/scala/resources/imdb-links.txt").getLines.toList
-    Extractor.extract(links, extractor).foreach(json => {
-
-    })
+    val movies = Extractor.extract(links, extractor)
+    Database.save(movies, "db/Movie/", ".json")
 }
 
 //noinspection SourceNotClosed
 object LNRunner extends App {
     val extractor = LaNacionExtractor()
     val links = Source.fromFile("src/main/scala/resources/la-nacion-links.txt").getLines.toList
-    Extractor.extract(links, extractor).foreach(json => {
-
-    })
+    val articles = Extractor.extract(links, extractor)
+    Database.save(articles, "db/NewsArticle/", ".json")
 }
